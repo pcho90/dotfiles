8316c06027631f45b4bf5a739a888da54ad02495
Peter Cho <pcho51990@gmail.com>
2022-06-26 00:37:08 -0400

    [nix] remove iterm symlink

--- darwin.nix
+++ darwin.nix
@@ -2,7 +2,6 @@
   programs.zsh.initExtra = ''
     VSCODE_SETTINGS=$HOME/Library/Application\ Support/Code/User/settings.json
     VSCODE_KEYBINDINGS=$HOME/Library/Application\ Support/Code/User/keybindings.json
-    ITERM_SETTINGS=$HOME/Library/Preferences/com.googlecode.iterm2.plist
     KARABINER_SETTINGS=$HOME/.config/karabiner
 
     if [[ ! -L $VSCODE_SETTINGS ]]; then
@@ -13,11 +12,6 @@
     if [[ ! -L $VSCODE_KEYBINDINGS ]]; then
       [[ -f $VSCODE_KEYBINDINGS ]] && rm $VSCODE_KEYBINDINGS
       ln -s $HOME/dotfiles/config/vscode/keybindings.json $VSCODE_KEYBINDINGS
-    fi
-
-    if [[ ! -L $ITERM_SETTINGS ]]; then
-      [[ -f $ITERM_SETTINGS ]] && rm $ITERM_SETTINGS
-      ln -s $HOME/dotfiles/config/iterm2/com.googlecode.iterm2.plist $ITERM_SETTINGS
     fi
 
     if [[ ! -L $KARABINER_SETTINGS ]]; then
