f7523a5923b92c408b642f5b242fc0dee61f127d
Peter Cho <peter.cho@discordapp.com>
2022-06-28 00:55:27 -0400

    [gpg] add conf

--- hosts/discord-mbp.nix
+++ hosts/discord-mbp.nix
@@ -5,10 +5,9 @@
     zsh = {
       initExtra = ''
         [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ] && source "$HOME/.nix-profile/etc/profile.d/nix.sh"
-      '';
 
-      envExtra = ''
-        export SSH_AUTH_SOCK=$HOME/.ssh/agent
+        export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+        gpg-connect-agent /bye
       '';
     };
   };
@@ -17,13 +16,6 @@
     "Library/LaunchAgents/com.zerowidth.launched.ssh_agent.plist".source = ../config/ssh/agent.plist;
 
     ".ssh/config".text = ''
-      Host *
-          AddKeysToAgent yes
-          IgnoreUnknown UseKeychain
-          UseKeychain yes
-          IdentityFile ~/.ssh/id_ed25519_sk
-
-
       Host coder.pcho90-dev
           HostName coder.pcho90-dev
           ProxyCommand /Users/peter.cho/.ssh/.coder-proxy-script-us-east-pcho90-dev.sh
