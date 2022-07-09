7a20521e937220268611051696a541a71852e5f4
Peter Cho <pcho51990@gmail.com>
2022-07-08 22:50:40 -0400

    [yabai] center window

---
+++ config/yabai/center.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PATH="/usr/local/bin:/opt/homebrew/bin:$HOME/.nix-profile/bin"
+
+window="$(yabai -m query --windows --window)"
+display="$(yabai -m query --displays --window)"
+coords="$(jq \
+--argjson window "${window}" \
+--argjson display "${display}" \
+-nr '(($display.frame | .x + .w / 2) - ($window.frame.w / 2) | tostring)
+    + ":"
+    + (($display.frame | .y + .h / 2) - ($window.frame.h / 2) | tostring)')"
+yabai -m window --move "abs:${coords}"
