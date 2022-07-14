3201bd828d851a1cd1109309c2d29e9c01c60d66
Peter Cho <peter.cho@discordapp.com>
2022-07-14 13:36:12 -0400

    [yabai] window border

--- hosts/discord-mbp.nix
+++ hosts/discord-mbp.nix
@@ -6,12 +6,6 @@
       initExtra = ''
         [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ] && source "$HOME/.nix-profile/etc/profile.d/nix.sh"
 
-        ITERM_SETTINGS=$HOME/.config/iterm2/com.googlecode.iterm2.plist
-        if [[ ! -L $ITERM_SETTINGS ]]; then
-          [[ -f $ITERM_SETTINGS ]] && rm $ITERM_SETTINGS
-          ln -s $HOME/dotfiles/config/iterm2/com.googlecode.iterm2.plist $ITERM_SETTINGS
-        fi
-
         export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
         gpg-connect-agent /bye
       '';
